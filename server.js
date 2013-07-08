1e13841536f08457c15ed45c84d4c91a1d9d15df
Todd Hooper <thoop3@gmail.com>
2013-07-07 18:56:24 -0600

    first commit with semi-working login server using UDP

---
+++ server.js
@@ -0,0 +1,42 @@
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('message', function(buffer, rinfo){
+    var hex = buffer.toString('hex');
+    var opcode = hex.substring(0,4);
+
+    if (opcode == '0001') {
+        console.log('R Session Request:' + hex + ' from ' + rinfo.address + ':' + rinfo.port);
+
+        var crcLength = hex.substring(4,12);
+        var connectionId = hex.substring(12,20);
+        var clientUDPSize = hex.substring(20,28);
+
+        var crcSeed = '00000000';
+        var crcLength = '02';
+        var useCompression = '00';
+        var seedSize = '04';
+
+        var response = new Buffer('0002' + connectionId + crcSeed + crcLength + useCompression + seedSize + clientUDPSize, 'hex');
+        console.log('S Session Response:' + response.toString('hex') + ' to ' + rinfo.address + ':' + rinfo.port);
+        server.send(response, 0, response.length, rinfo.port, rinfo.address);
+
+    else if (opcode == '0007') {
+        console.log('R Client Network Status Update:' + hex + ' from ' + rinfo.address + ':' + rinfo.port);
+
+        var response = new Buffer('0000000000000000000000000000000000000000000000000000000000000000000000000000', 'hex');
+        console.log('S Server Network Status Update:' + response.toString('hex') + ' to ' + rinfo.address + ':' + rinfo.port);
+        server.send(response, 0, response.length, rinfo.port, rinfo.address);
+
+    } else {
+        console.log('R UNKNOWN:' + hex + ' from ' + rinfo.address + ':' + rinfo.port);
+    }
+
+});
+
+server.on('listening', function(){
+    var address = server.address();
+    console.log('server listening ' + address.address + ':' + address.port);
+});
+
+server.bind(44453);
