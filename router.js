abeb5aad5189672532ddaffd304bbeb2a866cc76
Todd Hooper <thooper@rallydev.com>
2013-07-11 23:25:29 -0600

    moved req and res to applicaton and fixed routing

--- router.js
+++ router.js
@@ -4,8 +4,12 @@ var _  = require('lodash')
 exports = module.exports = Router
 
 function Router(options) {
+    var _this = this;
 	this.map = {};
-	this.options = options;
+	this.options = options || {};
+    this.middleware = function(req, res, next) {
+        _this._dispatch(req, res, next);
+    }
 }
 
 Router.prototype.register = function() {
@@ -28,119 +32,28 @@ Router.prototype.register = function() {
 	return this;
 }
 
-Router.prototype._dispatch = function(buffer, requestInfo){
-    var hex = buffer.toString('hex');
-    var opcode = hex.substring(0,4);
+Router.prototype._dispatch = function(req, res, next){
+    var opcode = req.hex.substring(0,4);
 
     if (this.options.verbose) {
-    	console.log('Recieved ' + hex + ' from ' + requestInfo.addrss + ':' + requestInfo.port);
+    	console.log('Recieved ' + req.hex + ' from ' + req.requestInfo.address + ':' + req.requestInfo.port);
     }
 
-    if (!this.map.opcode) {
-    	console.log('No route matched for ' + hex + '. Ignoring packet.');
+    if (!this.map[opcode]) {
+    	console.log('No route matched for ' + req.hex + '. Ignoring packet.');
     	return;
     }
 
-    var req = {};
-    var res = {};
-
-    _.each(this.map.opcode, function(route) {
+    _.each(this.map[opcode], function(route) {
     	var i = 1;
 
-    	route.callbacks[0].call(route, req, res, function() {
-    		if (!route.callbacks[i]) return;
+        var next = function() {
+            if (!route.callbacks[i]) return;
 
-    		route.callbacks[i++].call(this, req, res, next);
-    	});
+            route.callbacks[i++].call(this, req, res, next);
+        }
+
+    	route.callbacks[0].call(route, req, res, next);
 
     });
 }
-
-var decrypt = function(hex, crcSeed) {
-
-    crcSeed = parseInt(crcSeed, 16); //get int of hex for bitwise operations
-    data = hex.substring(4).substring(0, hex.length - 8); //ignore opcode and crc
-    var length = data.length;
-
-    var response = '', newHexValue, index, prevIndex;
-
-    var block_count = Math.floor(length / 8);
-    var byte_count = (length % 8 / 2); //divided by 2 since each hex number is 2 numbers
-
-    // for(var i =0; i < length; i+=8) {
-    //     prevIndex = i - 8;
-
-    //     crcSeed = prevIndex < 0 ? crcSeed : parseInt(data.substring(prevIndex, prevIndex+8), 16);
-    //     newHexValue = ((parseInt(data.substring(index, index+8), 16) ^ crcSeed)>>>0).toString(16); //>>>0 turns it into an unsigned int
-    //     while (newHexValue.length < 8) {
-    //         newHexValue = '0' + newHexValue;
-    //     }
-    //     response += newHexValue;
-    // }
-
-    for(var i = 0; i < block_count; i++) {
-        index = i*8;
-        prevIndex = (i-1)*8;
-
-        if(i > 0) {
-            crcSeed = parseInt(data.substring(prevIndex, prevIndex+8), 16);
-        }
-
-        newHexValue = ((parseInt(data.substring(index, index+8), 16) ^ crcSeed)>>>0).toString(16); //>>>0 turns it into an unsigned int
-        while (newHexValue.length < 8) {
-            newHexValue = '0' + newHexValue;
-        }
-        response += newHexValue;
-    }
-
-    crcSeed = parseInt(data.substring(data.length - byte_count*2 - 8, data.length - byte_count*2 - 6), 16); //get the first byte from the key
-    for(var j = 0; j < byte_count; j++) {
-        index = i*8+(j*2);
-
-        newHexValue = ((parseInt(data.substring(index, index+2), 16) ^ crcSeed)>>>0).toString(16);
-        if (newHexValue.length != 2) {
-            newHexValue = '0' + newHexValue;
-        }
-        response += newHexValue;
-    }
-
-    response = hex.substring(0,4) + response + hex.substring(hex.length - 4);
-    return response.toUpperCase();
-};
-
-var encrypt = function(hex, crcSeed) {
-
-    crcSeed = parseInt(crcSeed, 16); //get int of hex for bitwise operations
-    data = hex.substring(4).substring(0, hex.length - 8); //ignore opcode and crc
-    var length = data.length;
-
-    var response = '', newHexValue, index, prevIndex;
-
-    var block_count = Math.floor(length / 8);
-    var byte_count = (length % 8 / 2); //divided by 2 since each hex number is 2 numbers
-
-    for(var i = 0; i < block_count; i++) {
-        index = i*8;
-
-        crcSeed = (parseInt(data.substring(index, index+8), 16) ^ crcSeed)>>>0; //>>>0 turns it into an unsigned int
-        newHexValue =  crcSeed.toString(16);
-        while (newHexValue.length < 8) {
-            newHexValue = '0' + newHexValue;
-        }
-        response += newHexValue;
-    }
-
-    crcSeed = parseInt(crcSeed.toString(16).substring(0,2), 16); //get the first byte from the key
-    for(var j = 0; j < byte_count; j++) {
-        index = i*8+(j*2);
-
-        newHexValue= ((parseInt(data.substring(index, index+2), 16) ^ crcSeed)>>>0).toString(16);
-        if (newHexValue.length != 2) {
-            newHexValue = '0' + newHexValue;
-        }
-        response += newHexValue;
-    }
-
-    response = hex.substring(0,4) + response + hex.substring(hex.length - 4);
-    return response.toUpperCase();
-};
--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var app = swg({
 });
 
 app.on('0001', function(req, res, next) {
-    console.log('got 0001');
+    console.log('in app.on for 0001');
     next();
 
 }, function(req, res, next) {
-    console.log('in next');
+    console.log('in app.on next');
 
     var crcLength = req.hex.substring(4,12);
     var connectionId = req.hex.substring(12,20);
@@ -20,19 +20,19 @@ app.on('0001', function(req, res, next) {
     var useCompression = '00';
     var seedSize = '04';
 
-    var response = new Buffer('0002' + connectionId + crcSeed + crcLength + useCompression + seedSize + clientUDPSize, 'hex');
+    var response = '0002' + connectionId + crcSeed + crcLength + useCompression + seedSize + clientUDPSize
 
     res.send(response);
 });
 
-app.on('0007', function(req, res, next) {
-    console.log('got 0007');
-    next();
-}, function(req, res, next) {
+// app.on('0007', function(req, res, next) {
+//     console.log('in app.on for 0007');
+//     next();
+// }, function(req, res, next) {
 
-    var response = new Buffer('0000000000000000000000000000000000000000000000000000000000000000000000000000', 'hex');
-    res.send(response);
-});
+//     var response = new Buffer('0000000000000000000000000000000000000000000000000000000000000000000000000000', 'hex');
+//     res.send(response);
+// });
 
 
 app.listen(44453, function() {
