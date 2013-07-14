df906886127d7b9c98090d721f6fd88524159bd0
Todd Hooper <thoop3@gmail.com>
2013-07-14 17:13:00 -0600

    moving more stuff to swg.js

--- server.js
+++ server.js
@@ -6,16 +6,11 @@ var app = swg({
 
 //Session request
 app.on('0001', function(req, res, next) {
-    var crcLength = req.hex.substring(4,12);
-    var connectionId = req.hex.substring(12,20);
-    var clientUDPSize = req.hex.substring(20,28);
-
-    var crcSeed = '00000000';
     var crcLength = '02';
     var useCompression = '00';
     var seedSize = '04';
 
-    res.send('0002' + connectionId + crcSeed + crcLength + useCompression + seedSize + clientUDPSize);
+    res.send('0002' + req.packet.connectionId + req.packet.crcSeed + crcLength + useCompression + seedSize + req.packet.clientUDPSize);
 });
 
 //disconnect
@@ -25,12 +20,19 @@ app.on('0005', function(req, res, next) {
 
 //Client network stats update
 app.on('0007', function(req, res, next) {
-    res.send('0000000000000000000000000000000000000000000000000000000000000000000000000000');
+    res.send('00080000000000000000000000000000000000000000000000000000000000000000000000000000');
 });
 
-//Channel 0 - data packet
-app.on('0009', function(req, res, next) {
-
+app.on('41131F96', function(req, res, next) {
+    console.log(req.packet);
+    var operandCount = '0400';
+    var opcode = 'C696B2AA';
+    var sessionKeySize = '3C000000';
+    var sessionKey = '20000000150000000ED693DED2EFBF8EA1ACD2EE4C55BE305FBE230DB4AB58F962697967E8106ED3869B3A4A1A72A1FA8F96FF9FA5625A2911000000';
+    var userId = '0000000000';
+    var stringSize = '0500';
+    var username = '74686F6F70';
+    res.send('0009' + operandCount + opcode + sessionKeySize + sessionKey + userId + stringSize + username + '000000');
 });
 
 
